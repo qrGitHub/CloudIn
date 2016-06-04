ca268de8e97061dca6e6b4519bf41c90772e4171
zhangbingyin <zhangbingyin@cloudin.ren>
2016-06-04 12:55:26 +0800

    add variable 'db_size' and fix bug(lack of -P option in one command) in runSysbench.sh

--- mysql/test/sysbench/runSysbench.sh
+++ mysql/test/sysbench/runSysbench.sh
@@ -6,6 +6,7 @@ user=root
 passwd=111111
 
 db_name=sbtest
+db_size=5000000
 
 #print a log and then exit
 function EXIT() {
@@ -14,20 +15,20 @@ function EXIT() {
 }
 
 #create database
-mysql -u$user -p$passwd -h $host -e "drop database if exists $db_name;"
+mysql -u$user -p$passwd -h $host -P $port -e "drop database if exists $db_name;"
 mysql -u$user -p$passwd -h $host -P $port -e "create database $db_name;"
 [ $? -eq 0 ] || EXIT "Create database FAILED!"
 
 # prepare data
-time sysbench --oltp-table-size=5000000 --test=oltp --mysql-table-engine=innodb --mysql-host=$host --mysql-port=$port --mysql-user=$user --mysql-password=$passwd --mysql-db=$db_name prepare
+time sysbench --oltp-table-size=$db_size --test=oltp --mysql-table-engine=innodb --mysql-host=$host --mysql-port=$port --mysql-user=$user --mysql-password=$passwd --mysql-db=$db_name prepare
 [ $? -eq 0 ] || EXIT "Prepare data FAILED!"
 
 # start benchmark
-time sysbench --max-time=600 --max-requests=0 --num-threads=16 --oltp-table-size=5000000 --test=oltp --mysql-table-engine=innodb --mysql-host=$host --mysql-port=$port --mysql-user=$user --mysql-password=$passwd --mysql-db=$db_name run
+time sysbench --max-time=600 --max-requests=0 --num-threads=16 --oltp-table-size=$db_size --test=oltp --mysql-table-engine=innodb --mysql-host=$host --mysql-port=$port --mysql-user=$user --mysql-password=$passwd --mysql-db=$db_name run
 [ $? -eq 0 ] || EXIT "Start benchmark FAILED!"
 
 # cleanup environment
-sysbench --num-threads=16 --oltp-table-size=5000000 --test=oltp --mysql-table-engine=innodb --mysql-host=$host --mysql-port=$port --mysql-user=$user --mysql-password=$passwd --mysql-db=$db_name cleanup
+sysbench --num-threads=16 --oltp-table-size=$db_size --test=oltp --mysql-table-engine=innodb --mysql-host=$host --mysql-port=$port --mysql-user=$user --mysql-password=$passwd --mysql-db=$db_name cleanup
 [ $? -eq 0 ] || EXIT "Cleanup FAILED!"
 
 # delete database

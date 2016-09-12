e9d35ea921ed4ccedfc36c6f23b7b3ca60839108
zhangbingyin <zhangbingyin@cloudin.ren>
2016-09-12 17:39:05 +0800

    change the method of downloading ceph-deploy_1.5.32_all.deb

--- ceph/deploy_ceph/install_ceph-deploy.sh
+++ ceph/deploy_ceph/install_ceph-deploy.sh
@@ -2,6 +2,7 @@
 
 wget -q -O- 'https://download.ceph.com/keys/release.asc' | sudo apt-key add -
 echo deb http://download.ceph.com/debian-jewel $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph.list
-sudo apt-get update
+#sudo apt-get update
+wget http://download.ceph.com/debian-jewel/pool/main/c/ceph-deploy/ceph-deploy_1.5.32_all.deb -O /var/cache/apt/archives/ceph-deploy_1.5.32_all.deb
 dpkg -i /var/cache/apt/archives/ceph-deploy_1.5.32_all.deb
 #sudo apt-get install ceph-deploy

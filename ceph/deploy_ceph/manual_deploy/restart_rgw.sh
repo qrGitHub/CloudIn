c567bd745edc65c071f2833c9b90020e1611e0a7
zhangbingyin <zhangbingyin@cloudin.cn>
2017-12-20 17:54:25 +0800

    add options --no-cleanup and -h for restart_rgw.sh

--- ceph/deploy_ceph/manual_deploy/restart_rgw.sh
+++ ceph/deploy_ceph/manual_deploy/restart_rgw.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+case $1 in
+    --no-cleanup)
+        cleanup=0
+        ;;
+    -h)
+        printf "Usage:\n\t%s [--no-cleanup|-h]\n" "$0"
+        exit 1
+        ;;
+    *)
+        cleanup=1
+        ;;
+esac
+
 . common.sh
 
 rgw_id=rgw.$(hostname)
 pfn="radosgw --cluster=${cluster:-ceph} --id $rgw_id --setuser ceph --setgroup ceph"
 
 restart_process "$pfn"
-sleep 3
->/var/log/ceph/ceph-client.${rgw_id}.log
+
+if [ $cleanup -eq 1 ]; then
+    sleep 3
+    >/var/log/ceph/ceph-client.${rgw_id}.log
+fi

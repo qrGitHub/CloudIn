2c333ec19cbfaab4a8db9d0e4d1a883386397c4f
zhangbingyin <zhangbingyin@cloudin.cn>
2017-12-20 17:52:52 +0800

    read endpoint from environment varialbe

--- ceph/rgw/s3/usage/boto3_bucketLifecycle.py
+++ ceph/rgw/s3/usage/boto3_bucketLifecycle.py
@@ -39,9 +39,9 @@ def print_dict(dictionary):
     pp = pprint.PrettyPrinter(indent=1, width=80, depth=None, stream=None)
     pp.pprint(dictionary)
 
-def put_bucket_lifecycle(bucket_name, bucket_policy):
+def put_bucket_lifecycle(bucket_name, lifecycle):
     # Set the new lifecycle on the given bucket
-    return s3client.put_bucket_lifecycle(Bucket=bucket_name, LifecycleConfiguration=bucket_policy)
+    return s3client.put_bucket_lifecycle(Bucket=bucket_name, LifecycleConfiguration=lifecycle)
 
 def get_bucket_lifecycle(bucket_name):
     # Call to S3 to retrieve the lifecycle for the given bucket
@@ -51,17 +51,18 @@ def get_bucket_lifecycle(bucket_name):
     pp = pprint.PrettyPrinter()
     pp.pprint(result)
 
-def del_bucket_policy(bucket_name):
+def del_bucket_lifecycle(bucket_name):
     # Call S3 to delete the lifecycle for the given bucket
     print s3client.delete_bucket_lifecycle(Bucket=bucket_name)
 
+endpoint = 'http://{0}'.format(os.environ.get('AWS_HOST'))
 # creating a client
 s3client = boto3.client('s3',
                         aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY'),
                         aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID'),
-                        endpoint_url = 'http://172.16.1.4:7480')
+                        endpoint_url = endpoint)
 
 #print_dict(bucket_lifecycle_list)
 put_bucket_lifecycle(bucket_name, bucket_lifecycle_list['demo1'])
 get_bucket_lifecycle(bucket_name)
-#del_bucket_policy(bucket_name)
+#del_bucket_lifecycle(bucket_name)

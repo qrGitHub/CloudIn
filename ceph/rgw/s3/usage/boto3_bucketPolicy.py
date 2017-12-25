ac6980dbab46247d014d1f4d9e7a220a54158acb
zhangbingyin <zhangbingyin@cloudin.cn>
2017-12-25 19:20:11 +0800

    1. move bucket policy list to file bucket_policy_list.py; 2. add bucket policy SpecificPrincipal; 3. excludes pyc files;

--- ceph/rgw/s3/usage/boto3_bucketPolicy.py
+++ ceph/rgw/s3/usage/boto3_bucketPolicy.py
@@ -1,141 +1,9 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+from bucket_policy_list import bucket_policy_list
 import boto3, json
 import pprint, os
-
-bucket_name = 'lyb'
-bucket_policy_list = {
-        'AnonymousRead': {
-            'Version':'2012-10-17',
-            'Statement':[{
-                'Sid':'AnonymousRead',
-                'Effect':'Allow',
-                'Principal': '*',
-                'Action':['s3:GetObject'],
-                'Resource':['arn:aws:s3:::%s/*' % bucket_name ]
-            }]
-        },
-        'PreventHotLinkingAllowNull': {
-            'Version': '2012-10-17',
-            'Id': 'PreventHotLinking',
-            'Statement': [
-                {
-                    'Sid': 'Allow get requests referred by 192.168.63.233',
-                    'Effect': 'Allow',
-                    'Principal': '*',
-                    'Action': ['s3:GetObject'],
-                    'Resource': [
-                        'arn:aws:s3:::%s/*' % bucket_name
-                    ],
-                    'Condition': {
-                        'StringLike': {'aws:Referer': ['http://192.168.63.23*']},
-                        'StringNotLike': {'aws:Referer': ['http://192.168.63.233*']}
-                    }
-                },
-                {
-                    'Sid': 'Explicit deny to ensure requests are allowed only from specific referer',
-                    'Effect': 'Deny',
-                    'Principal': '*',
-                    'Action': 's3:*',
-                    'Resource': 'arn:aws:s3:::%s/*' % bucket_name,
-                    'Condition': {
-                        'StringNotLike': {'aws:Referer': ['http://192.168.63.23*']}
-                    }
-                }
-            ]
-        },
-        'PreventHotLinkingDenyNull1': {
-            'Version': '2012-10-17',
-            'Id': 'PreventHotLinking',
-            'Statement': [
-                {
-                    'Sid': 'Allow get requests referred by 192.168.63.233',
-                    'Effect': 'Allow',
-                    'Principal': '*',
-                    'Action': ['s3:GetObject'],
-                    'Resource': [
-                        'arn:aws:s3:::%s/*' % bucket_name
-                    ],
-                    'Condition': {
-                        'StringLike': {'aws:Referer': ['http://192.168.63.23*']},
-                    }
-                },
-                {
-                    'Sid': 'Explicit deny to ensure requests are allowed only from specific referer',
-                    'Effect': 'Deny',
-                    'Principal': '*',
-                    'Action': 's3:GetObject',
-                    'Resource': 'arn:aws:s3:::%s/*' % bucket_name,
-                    'Condition': {
-                        "Null": {"aws:Referer": "true"}
-                    }
-                }
-            ]
-        },
-        'PreventHotLinkingDenyNull2': {
-            'Version': '2012-10-17',
-            'Id': 'PreventHotLinking',
-            'Statement': [
-                {
-                    'Sid': 'Allow get requests referred by 192.168.63.233',
-                    'Effect': 'Allow',
-                    'Principal': '*',
-                    'Action': ['s3:GetObject'],
-                    'Resource': [
-                        'arn:aws:s3:::%s/*' % bucket_name
-                    ],
-                    'Condition': {
-                        'StringLike': {'aws:Referer': ['http://192.168.63.23*']},
-                    }
-                },
-                {
-                    'Sid': 'Explicit deny to ensure requests are allowed only from specific referer',
-                    'Effect': 'Deny',
-                    'Principal': '*',
-                    'Action': 's3:GetObject',
-                    'Resource': 'arn:aws:s3:::%s/*' % bucket_name,
-                    'Condition': {
-                        'StringNotLikeIfExists': {'aws:Referer': ['http://192.168.63.23*']}
-                    }
-                }
-            ]
-        },
-        'SpecificIPv4': {
-            'Version': '2012-10-17',
-            'Id': 'SpecificIPv4',
-            'Statement': [
-                {
-                    'Sid': 'IPAllow',
-                    'Effect': 'Allow',
-                    'Principal': '*',
-                    'Action': 's3:GetObject',
-                    'Resource': 'arn:aws:s3:::%s/*' % bucket_name,
-                    'Condition': {
-                        'IpAddress': {'aws:SourceIp': ['10.3.0.0/24', '172.16.1.5/32']},
-                        'NotIpAddress': {'aws:SourceIp': '10.3.0.101/32'}
-                    }
-                }
-            ]
-        },
-        'match_wildcards_bug': {
-            'Version': '2012-10-17',
-            'Id': 'match_wildcards_bug',
-            'Statement': [
-                {
-                    'Sid': 'Explicit deny for specific referer',
-                    'Effect': 'Deny',
-                    'Principal': '*',
-                    'Action': 's3:GetObject',
-                    'Resource': 'arn:aws:s3:::%s/*' % bucket_name,
-                    'Condition': {
-                        'StringLike': {'aws:Referer': ['http://192.*32*']}
-                        #'StringLike': {'aws:Referer': ['http://192.168.63.232*']}
-                    }
-                }
-            ]
-        },
-}
 
 def print_dict(dictionary):
     pp = pprint.PrettyPrinter(indent=1, width=80, depth=None, stream=None)
@@ -164,6 +32,8 @@ def del_bucket_policy(bucket_name):
     # Call S3 to delete the policy for the given bucket
     print s3client.delete_bucket_policy(Bucket=bucket_name)
 
+bucket_name = 'lyb'
+
 # creating a client
 s3client = boto3.client('s3',
                         aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY'),
@@ -172,6 +42,6 @@ s3client = boto3.client('s3',
 
 #print_dict(bucket_policy_list)
 #dict2string(bucket_policy_list)
-put_bucket_policy(bucket_name, bucket_policy_list['AnonymousRead']) # PreventHotLinkingAllowNull PreventHotLinkingDenyNull[1-2] AnonymousRead SpecificIPv4
-#get_bucket_policy(bucket_name)
+put_bucket_policy(bucket_name, bucket_policy_list['SpecificUser']) # PreventHotLinkingAllowNull PreventHotLinkingDenyNull[1-2] AnonymousRead SpecificIPv4 SpecificPrincipal
+get_bucket_policy(bucket_name)
 #del_bucket_policy(bucket_name)
---
+++ ceph/rgw/s3/usage/bucket_policy_list.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+bucket_name = 'lyb'
+bucket_policy_list = {
+        'SpecificUser': {
+            'Version':'2012-10-17',
+            'Statement':[{
+                'Sid':'SpecificPrincipal',
+                'Effect':'Allow',
+                'Principal': {'AWS': ['arn:aws:iam:::user/normal']},
+                'Action':['s3:GetObject'],
+                'Resource':['arn:aws:s3:::%s/*' % bucket_name ]
+            }]
+        },
+        'AnonymousRead': {
+            'Version':'2012-10-17',
+            'Statement':[{
+                'Sid':'AnonymousRead',
+                'Effect':'Allow',
+                'Principal': '*',
+                'Action':['s3:GetObject'],
+                'Resource':['arn:aws:s3:::%s/*' % bucket_name ]
+            }]
+        },
+        'SpecificIPv4': {
+            'Version': '2012-10-17',
+            'Id': 'SpecificIPv4',
+            'Statement': [
+                {
+                    'Sid': 'SpecificIPv4',
+                    'Effect': 'Allow',
+                    'Principal': '*',
+                    'Action': 's3:GetObject',
+                    'Resource': 'arn:aws:s3:::%s/*' % bucket_name,
+                    'Condition': {
+                        'IpAddress': {'aws:SourceIp': ['10.3.0.0/24', '172.16.1.5/32']},
+                        'NotIpAddress': {'aws:SourceIp': '10.3.0.101/32'}
+                    }
+                }
+            ]
+        },
+        'PreventHotLinkingAllowNull': {
+            'Version': '2012-10-17',
+            'Id': 'PreventHotLinking',
+            'Statement': [
+                {
+                    'Sid': 'Allow get requests referred by 192.168.63.233',
+                    'Effect': 'Allow',
+                    'Principal': '*',
+                    'Action': ['s3:GetObject'],
+                    'Resource': [
+                        'arn:aws:s3:::%s/*' % bucket_name
+                    ],
+                    'Condition': {
+                        'StringLike': {'aws:Referer': ['http://192.168.63.23*']},
+                        'StringNotLike': {'aws:Referer': ['http://192.168.63.233*']}
+                    }
+                },
+                {
+                    'Sid': 'Explicit deny to ensure requests are allowed only from specific referer',
+                    'Effect': 'Deny',
+                    'Principal': '*',
+                    'Action': 's3:*',
+                    'Resource': 'arn:aws:s3:::%s/*' % bucket_name,
+                    'Condition': {
+                        'StringNotLike': {'aws:Referer': ['http://192.168.63.23*']}
+                    }
+                }
+            ]
+        },
+        'PreventHotLinkingDenyNull1': {
+            'Version': '2012-10-17',
+            'Id': 'PreventHotLinking',
+            'Statement': [
+                {
+                    'Sid': 'Allow get requests referred by 192.168.63.233',
+                    'Effect': 'Allow',
+                    'Principal': '*',
+                    'Action': ['s3:GetObject'],
+                    'Resource': [
+                        'arn:aws:s3:::%s/*' % bucket_name
+                    ],
+                    'Condition': {
+                        'StringLike': {'aws:Referer': ['http://192.168.63.23*']},
+                    }
+                },
+                {
+                    'Sid': 'Explicit deny to ensure requests are allowed only from specific referer',
+                    'Effect': 'Deny',
+                    'Principal': '*',
+                    'Action': 's3:GetObject',
+                    'Resource': 'arn:aws:s3:::%s/*' % bucket_name,
+                    'Condition': {
+                        "Null": {"aws:Referer": "true"}
+                    }
+                }
+            ]
+        },
+        'PreventHotLinkingDenyNull2': {
+            'Version': '2012-10-17',
+            'Id': 'PreventHotLinking',
+            'Statement': [
+                {
+                    'Sid': 'Allow get requests referred by 192.168.63.233',
+                    'Effect': 'Allow',
+                    'Principal': '*',
+                    'Action': ['s3:GetObject'],
+                    'Resource': [
+                        'arn:aws:s3:::%s/*' % bucket_name
+                    ],
+                    'Condition': {
+                        'StringLike': {'aws:Referer': ['http://192.168.63.23*']},
+                    }
+                },
+                {
+                    'Sid': 'Explicit deny to ensure requests are allowed only from specific referer',
+                    'Effect': 'Deny',
+                    'Principal': '*',
+                    'Action': 's3:GetObject',
+                    'Resource': 'arn:aws:s3:::%s/*' % bucket_name,
+                    'Condition': {
+                        'StringNotLikeIfExists': {'aws:Referer': ['http://192.168.63.23*']}
+                    }
+                }
+            ]
+        },
+        'match_wildcards_bug': {
+            'Version': '2012-10-17',
+            'Id': 'match_wildcards_bug',
+            'Statement': [
+                {
+                    'Sid': 'Explicit deny for specific referer',
+                    'Effect': 'Deny',
+                    'Principal': '*',
+                    'Action': 's3:GetObject',
+                    'Resource': 'arn:aws:s3:::%s/*' % bucket_name,
+                    'Condition': {
+                        'StringLike': {'aws:Referer': ['http://192.*32*']}
+                        #'StringLike': {'aws:Referer': ['http://192.168.63.232*']}
+                    }
+                }
+            ]
+        },
+}
